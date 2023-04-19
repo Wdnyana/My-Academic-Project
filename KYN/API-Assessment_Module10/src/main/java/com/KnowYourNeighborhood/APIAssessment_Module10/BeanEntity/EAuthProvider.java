8e13a33d291981c41ee7df366c05501aeae71791
Purwa Wiadnyana <63180557+Wdnyana@users.noreply.github.com>
2023-04-19 19:11:06 +0800

    delete kyn project

--- KYN/API-Assessment_Module10/src/main/java/com/KnowYourNeighborhood/APIAssessment_Module10/BeanEntity/EAuthProvider.java
+++
@@ -1,9 +0,0 @@
-package com.KnowYourNeighborhood.APIAssessment_Module10.BeanEntity;
-
-public enum EAuthProvider {
-
-    local,
-    google,
-    facebook
-
-}
--- KYN/API-Assessment_Module10/src/main/java/com/KnowYourNeighborhood/APIAssessment_Module10/BeanEntity/ERole.java
+++
@@ -1,6 +0,0 @@
-package com.KnowYourNeighborhood.APIAssessment_Module10.BeanEntity;
-
-public enum ERole {
-    ROLE_USER,
-    ROLE_ADMIN
-}
