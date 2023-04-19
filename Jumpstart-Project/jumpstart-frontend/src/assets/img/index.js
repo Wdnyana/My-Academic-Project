e73e6ea325a1e7940cd316c6646d3f7d14f08e58
Purwa Wiadnyana <63180557+Wdnyana@users.noreply.github.com>
2023-04-19 18:58:22 +0800

    delete

--- Jumpstart-Project/jumpstart-frontend/src/assets/img/index.js
+++
@@ -1,5 +0,0 @@
-import AboutImg_1 from "./img-about-1.jpg";
-import AboutImg_2 from "./img-about-2.jpg";
-import HeroImg from "./img-hero-section.jpg";
-
-export { AboutImg_1, AboutImg_2, HeroImg };
--- Jumpstart-Project/jumpstart-frontend/src/components/admin/index.js
+++
@@ -1,6 +0,0 @@
-import Categories from "./categories";
-import Products from "./products";
-import Orders from "./orders";
-import DashboardAdmin from "./dashboardAdmin";
-
-export { DashboardAdmin, Categories, Products, Orders };
